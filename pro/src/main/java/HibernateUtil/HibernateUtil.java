860704d536c311281f19831232753c313526697d
Roman Orlov <orlov.romanv@yandex.ru>
2016-11-28 23:58:48 +0400

    user

--- pro/src/main/java/HibernateUtil/HibernateUtil.java
+++ pro/src/main/java/HibernateUtil/HibernateUtil.java
@@ -7,19 +7,15 @@ import org.hibernate.cfg.Configuration;
  * Created by Роман on 07.11.2016.
  */
 public class HibernateUtil {
-
-
-
-
     public static SessionFactory getSessionFactory() {
         try {
             // Create the SessionFactory from hibernate.cfg.xml
             SessionFactory sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
             return  sessionFactory;
-        } catch (Throwable ex) {
+       } catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
+           System.err.println("Initial SessionFactory creation failed." + ex);
+           throw new ExceptionInInitializerError(ex);
         }
 
 
